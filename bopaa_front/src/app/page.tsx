01a6c079e9d76720f520a01a516802f1428569ed
Guada-Chojo <gchojohernandez@gmail.com>
2024-10-06 19:29:10 -0300

    Tailwind CSS instalado

--- bopaa_front/src/app/page.tsx
+++ bopaa_front/src/app/page.tsx
@@ -15,6 +15,9 @@ export default function Home() {
         />
         <ol>
           <li>
+            <h1 className="text-3xl font-bold underline">
+              Hello world!
+            </h1>
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
