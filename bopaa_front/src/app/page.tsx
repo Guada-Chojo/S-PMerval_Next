04988162c559e627c75744ad3c5e62973eaf5d0e
Guada-Chojo <gchojohernandez@gmail.com>
2024-12-04 14:06:29 -0300

    fix

--- bopaa_front/src/app/page.tsx
+++ bopaa_front/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
+import { Footer } from "./components/Footer/footer";
 import { NavBar } from "./components/navBar/navBar";
-import { SideBar } from "./components/sideBar/sideBar";
-import { Footer } from "./components/footer/footer";
+import { SideBar } from "./components/SideBar/sideBar";
 import { CurrencyProvider } from "./context/currency.context";
 
 export default function Home() {
